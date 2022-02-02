4f90550c2b571ee0f623e5a1824c04b32a562b1d
MasrukAhmed <95853101+masrukAhmedMurad@users.noreply.github.com>
2022-02-02 22:29:09 +0600

    Update arrayOperation.c

--- arrayOperation.c
+++ arrayOperation.c
@@ -7,6 +7,7 @@ int main(){
 int arrayOne[]={12,22,34,23,56,76,34,23,45,34};
 int deleteIndex;
 int n=10;  //number of elements in array
+int i;
 
 
 scanf("%d",&deleteIndex);//get input delete index which index element you want to delete;
@@ -17,7 +18,7 @@ if(deleteIndex>=n || deleteIndex<0){
     printf("Deletion is not possible\n");
 }else{
 
-for(int i=deleteIndex;i<n-1;i++){
+for(i=deleteIndex;i<n-1;i++){
     arrayOne[i]=arrayOne[i+1];
 }
 
@@ -31,7 +32,7 @@ n--;
 
 //print the array
 
-for(int i=0;i<n;i++){
+for(i=0;i<n;i++){
     printf("%d  ",arrayOne[i]);
 }
 
